a1e302a1b857898e263aab8895e9a83d926988f7
Luccas Carneiro <luccas.carneiro@terceiroslinx.com.br>
2025-08-04 12:57:27 -0300

    Updates

--- ClienteChat/ClienteChat.cs
+++ ClienteChat/ClienteChat.cs
@@ -77,6 +77,7 @@ namespace Chat_TCP
             var broadcastIp = IPAddress.Parse("192.168.1.255");
             // envia para o listener de discovery do servidor (porta 30001)
             udp.Send(payload, payload.Length, new IPEndPoint(broadcastIp, 30001));
+            //udp.Send(payload, payload.Length, new IPEndPoint(IPAddress.Broadcast, 30001));
 
             try
             {
